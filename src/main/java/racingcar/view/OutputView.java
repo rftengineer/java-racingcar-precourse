f0142e79c38821830c0d48e15f428c33cf1aa023
Steve Shim <nightpw1@gmail.com>
2022-04-24 20:32:57 +0900

    refactor(View): String에 대해서 상수 선언부를 가져다 쓰도록 리팩토링

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,8 +1,12 @@
 package racingcar.view;
 
 public class OutputView {
+
+    private static final String WINNER_SENTENCE = "최종 우승자: " ;
+    private static final String HEADLINE = "실행 결과" ;
+
     private static void printHeadLine() {
-        System.out.println("실행 결과");
+        System.out.println(HEADLINE);
     }
 
     private static void printScores(String scores) {
@@ -10,7 +14,7 @@ public class OutputView {
     }
 
     private static void printWinner(String winners) {
-        System.out.println("최종 우승자: " + winners);
+        System.out.println(WINNER_SENTENCE + winners);
     }
 
     public static void resultPrintOut(String scores, String winners) {
