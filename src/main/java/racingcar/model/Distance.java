45a22253c26062b55b716815d2321555c69547a1
Steve Shim <nightpw1@gmail.com>
2022-04-24 14:27:49 +0900

    feat(Distance): 원시값(이동거리) 포장 객체 생성 및 기본 Test만 구현

---
+++ src/main/java/racingcar/model/Distance.java
@@ -0,0 +1,17 @@
+package racingcar.model;
+
+public class Distance {
+    private Integer distance;
+
+    public Distance(int value) {
+        this.distance = value;
+    }
+
+    public int getValue() {
+        return 0;
+    }
+
+    public void add() {
+        this.distance++;
+    }
+}
---
+++ src/test/java/racingcar/model/DistanceTest.java
@@ -0,0 +1,28 @@
+package racingcar.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class DistanceTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,1", "3,3"})
+    @DisplayName("기본적인 Distance 인스턴스 생성 및 정상 값 적재 확인")
+    void createDistanceAndValueTest(int input, int expected) {
+        Distance distance = new Distance(input);
+        assertThat(distance.getValue()).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1, 2, 3", "2, 4, 6"})
+    void increaseDistanceWithAddMethod(int initial, int loopCnt, int expected) {
+        Distance distance = new Distance(initial);
+        for(int loop = 0; loop < loopCnt; loop++){
+            distance.add();
+        }
+        assertThat(distance.getValue()).isEqualTo(expected);
+    }
+}
