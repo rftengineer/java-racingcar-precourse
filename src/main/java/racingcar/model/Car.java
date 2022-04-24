821c1fee2e82cef3c2bc52c6717b760eb644fcb7
Steve Shim <nightpw1@gmail.com>
2022-04-24 14:52:33 +0900

    feat(Car): 요구사항 구현을 위한 Car 객체 구현 및 기본적인 테스트 구현

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,22 @@
+package racingcar.model;
+
+public class Car {
+    private final Name name;
+    private final Distance distance;
+
+    public Car(String name, int initialDistance) {
+        this.name = new Name(name);
+        this.distance = new Distance(initialDistance);
+    }
+
+    public void move(int decision) {
+        if (decision >= 4) {
+            distance.add();
+        }
+    }
+
+    public String showResult() {
+        return name.getName() + ": " + distance.getValue();
+    }
+
+}
